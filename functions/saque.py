a016ef5bac90b74021b69c0c8eba1064c3e81ebb
BrunoRafael14 <brunorafaelfilho10@gmail.com>
2025-12-24 20:59:08 -0300

    Adiciona função de Saque

--- functions/saque.py
+++ functions/saque.py
@@ -1,2 +1,15 @@
-def fazer_saque():
-    pass
+def fazer_saque(*, valor, extrato, saldo, limite, numero_saques, LIMITE_SAQUES):
+    if valor > saldo:
+        print("Operação falhou! você não tem saldo suficiente. ")
+    elif valor > limite:
+        print("Operação falhou! O valor do saque excede o limite.")
+    elif numero_saques > LIMITE_SAQUES:
+        print("Operação falhou! Número máximo de saques excedido.")
+    elif valor > 0:
+        saldo -= valor
+        extrato += f"Saque: R$ {valor:.2f}\n"
+        numero_saques += 1
+    else: 
+        print("Operação falhou! O valor informado é inválido.")
+
+    return saldo, extrato, numero_saques
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from functions.deposito import fazer_deposito
 from functions.extrato import puxar_extrato
+from functions.saque import fazer_saque
 
 menu = """
 
@@ -21,36 +22,15 @@ while True:
     opcao = input(menu)
 
     if opcao == "d":
-        while True:
-            valor = float(input("Informe o valor do Deposito: "))
-            saldo, extrato = fazer_deposito(valor, saldo, extrato)
-            break
+        valor = float(input("Informe o valor do Deposito: "))
+        
+        saldo, extrato = fazer_deposito(valor, saldo, extrato)
 
     elif opcao == "s":
         valor = float(input("Informe o valor do saque: "))
 
-        excedeu_saldo = valor > saldo
+        saldo, extrato, numero_saques = fazer_saque(valor=valor, extrato=extrato, saldo=saldo, limite=limite, numero_saques=numero_saques, LIMITE_SAQUES=LIMITE_SAQUES)
 
-        excedeu_limite = valor > limite
-
-        excedeu_saques = numero_saques >= LIMITE_SAQUES
-
-        if excedeu_saldo:
-            print("Operação falhou! Você não tem saldo suficiente.")
-
-        elif excedeu_limite:
-            print("Operação falhou! O valor do saque excede o limite.")
-
-        elif excedeu_saques:
-            print("Operação falhou! Número máximo de saques excedido.")
-
-        elif valor > 0:
-            saldo -= valor
-            extrato += f"Saque: R$ {valor:.2f}\n"
-            numero_saques += 1
-
-        else:
-            print("Operação falhou! O valor informado é inválido.")
 
     elif opcao == "e":
         puxar_extrato(extrato, saldo=saldo)
