f564f1a0ba6b347adc04cf64721be1822f3f3aba
BrunoRafael14 <brunorafaelfilho10@gmail.com>
2026-01-03 10:48:33 -0300

    correção de bug envolvendo função de saque

--- functions/operacoes/saque.py
+++ functions/operacoes/saque.py
@@ -3,14 +3,17 @@ def fazer_saque(*, valor, contas, status_login):
         if int(c["Número da Conta"]) == status_login["Conta Ativa"]:
             if valor > c["saldo"]:
                 print("Operação falhou! você não tem saldo suficiente. ")
+                return
             elif valor > c["limite"]:
                 print("Operação falhou! O valor do saque excede o limite.")
-            elif c["numero_saques"] > c["LIMITE_SAQUES"]:
+                return
+            elif c["numero_saques"] >= c["LIMITE_SAQUES"]:
                 print("Operação falhou! Número máximo de saques excedido.")
+                return
             elif valor > 0:
                 c["saldo"] -= valor
                 c["extrato"] += f"Saque: R$ {valor:.2f}\n"
                 c["numero_saques"] += 1
+                return
          
-        else: 
-            print("Operação falhou! O valor informado é inválido.")
+    print("Operação falhou! O valor informado é inválido.")
